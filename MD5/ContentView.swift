0d83a66c3969fd469bdd5fa8a3782547b190cd5e
Behdad Keynejad <behdad.keynejad@gmail.com>
2020-10-30 23:59:07 +0330

    Initial Commit

---
+++ MD5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MD5
+//
+//  Created by Behdad Keynejad on 8/9/1399 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MD5/MD5App.swift
@@ -0,0 +1,17 @@
+//
+//  MD5App.swift
+//  MD5
+//
+//  Created by Behdad Keynejad on 8/9/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct MD5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
